e194cd463ff8cb78b8c9f7fc106185b008cf2bd8
Jesse Newland <jnewland@gmail.com>
2008-03-24 08:59:36 -0400

    i'm a dumbass

--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -6,7 +6,7 @@ describe "The plugin" do
     @user = User.create
   end
  
-  describe "includes instances methods when calling ride_the_fireeagle" do
+  it "includes instances methods when calling ride_the_fireeagle" do
     @user.respond_to?(:location).should == true
   end
   
